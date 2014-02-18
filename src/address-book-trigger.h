56689728298a7549ac2027c41eaa116afe092cf5
Renato Araujo Oliveira Filho <renato.filho@canonical.com>
2014-02-18 09:07:56 -0300

    Added license headers.

--- src/address-book-trigger.h
+++ src/address-book-trigger.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2014 Canonical Ltd.
+ *
+ * This file is part of sync-monitor.
+ *
+ * sync-monitor is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * contact-service-app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __ADDRESS_BOOK_TRIGGER_H__
 #define __ADDRESS_BOOK_TRIGGER_H__
 
