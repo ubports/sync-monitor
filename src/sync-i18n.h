5dbe295e95a5b673a943bd7d14e4e160bd8c837e
Renato Araujo Oliveira Filho <renato.filho@canonical.com>
2014-05-09 11:47:49 -0300

    Added missing file.

---
+++ src/sync-i18n.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2014 Canonical Ltd.
+ *
+ * This file is part of sync-monitor.
+ *
+ * sync-monitor is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * contact-service-app is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __SYNC_I18N_H__
+#define __SYNC_I18N_H__
+
+#include "config.h"
+
+extern "C" {
+#include <libintl.h>
+#define _(String) dgettext (GETTEXT_PACKAGE, String)
+}
+
+#endif
